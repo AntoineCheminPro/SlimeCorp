9d38002a32f7e015d3422eb086e37495e20ad92b
Antoine Chemin <67341298+AntoineCheminPro@users.noreply.github.com>
2021-11-13 17:25:57 +0100

    routing done & 404 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientsComponent } from './clients/clients.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component : HomePageComponent},
+  { path: "clients", component : ClientsComponent},
+  { path: "services", component : ServicesComponent},
+  { path: "projets", component : ProjetsComponent},
+  { path: "**", component: FourOhFourComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HeadComponent } from './head/head.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
-import { HeadComponent } from './head/head.component';
 import { ServicesComponent } from './services/services.component';
 import { ProjetsComponent } from './projets/projets.component';
 import { ClientsComponent } from './clients/clients.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeadComponent,
     HeaderComponent,
     NavComponent,
     FooterComponent,
-    HeadComponent,
     ServicesComponent,
     ProjetsComponent,
-    ClientsComponent
+    ClientsComponent,
+    HomePageComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
